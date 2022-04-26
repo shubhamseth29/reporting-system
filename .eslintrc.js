81f370c6b4d5999f8a962f14b85b460fcb250894
Shubham Seth <Shubham.seth@centilytics.com>
2022-04-26 18:14:57 +0530

    SUBJECT: Linting , Hooks , Extension DONE MESSAGE: Git hooks are added in .githooks folder , to run it a cli command is added as comment in all hooks Extensions added in .vscode folder and same for settings

--- .eslintrc.js
+++ .eslintrc.js
@@ -27,7 +27,55 @@ module.exports = {
       {
         'endOfLine': 'auto',
       }
-    ]
+    ],
+    
+
+        // checks for console log, err, etc.
+        // https://eslint.org/docs/rules/no-console
+        'no-console': 'error',
+
+        // require let or const instead of var
+        'no-var': 'error',
+
+        // require the use of === and !==
+        // https://eslint.org/docs/rules/eqeqeq
+        eqeqeq: ['warn', 'always'],
+
+        // Disallow returning value in constructor
+        // https://eslint.org/docs/rules/no-constructor-return
+        // TODO: enable, semver-major
+        'no-constructor-return': 'error',
+
+        // disallow use of multiple spaces
+        'no-multi-spaces': [
+            'warn',
+            {
+                ignoreEOLComments: false,
+            },
+        ],
+
+        // disallow to use this/super before super() calling in constructors.
+        // https://eslint.org/docs/rules/no-this-before-super
+        'no-this-before-super': 'error',
+        // suggest using arrow functions as callbacks
+        'prefer-arrow-callback': [
+            'error',
+            {
+                allowNamedFunctions: false,
+                allowUnboundThis: true,
+            },
+        ],
+
+        // suggest using of const declaration for variables that are never modified after declared
+        'prefer-const': [
+            'error',
+            {
+                destructuring: 'any',
+                ignoreReadBeforeAssign: true,
+            },
+        ],
+         // treat var statements as if they were block scoped
+        'block-scoped-var': 'error',
       
   },
 };
